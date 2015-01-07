d368a4af0eb3714d21e13405625fbf173b3ebfc1
hardincm2 <hardincm2@gmail.com>
2015-01-07 01:30:19 -0800

    initial commit

---
+++ desktop/src/com/brassbeluga/momentum/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.brassbeluga.momentum.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.brassbeluga.momentum.Momentum;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Momentum(), config);
+	}
+}
---
+++ html/src/com/brassbeluga/momentum/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.brassbeluga.momentum.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.brassbeluga.momentum.Momentum;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Momentum();
+        }
+}
